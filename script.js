c0c8194564e12fbf3bd5023e9e99129a2e315940
David Shonn <davidlshonn@icloud.com>
2020-11-21 18:42:39 +0000

    Add time block checksto style with given css for past, present, future

--- script.js
+++ script.js
@@ -2,18 +2,34 @@ $(document).ready(function () {
   var currentDateandTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
   $("#currentDay").text(currentDateandTime);
 
-  $(".saveBtn").on("click", function(){
-      var userCalendarTask = $(this).siblings(".description").val();
-      console.log(userCalendarTask);
+  $(".saveBtn").on("click", function () {
+    var userCalendarTask = $(this).siblings(".description").val();
+    console.log(userCalendarTask);
     var timeBlockId = $(this).parent().attr("id");
-   console.log(timeBlockId)
-    localStorage.setItem(timeBlockId, userCalendarTask)
-  })
+    console.log(timeBlockId);
+    localStorage.setItem(timeBlockId, userCalendarTask);
+  });
 
-
-  var taskFor8 = localStorage.getItem("8")
+  var taskFor8 = localStorage.getItem("8");
   $("#8 .description").val(taskFor8);
-  var taskFor9 = localStorage.getItem("9")
+  var taskFor9 = localStorage.getItem("9");
   $("#9 .description").val(taskFor9);
 
+
+
+  //Checking the time in the time blocks to add the relevant css to them
+  function checkTimeStatus() {
+    var presentHour = moment().hours();
+    $(".time-block").each(function () {
+      var thisTimeBlockId = $(this).attr("id");
+      if (thisTimeBlockId < presentHour) {
+        $(this).addClass("past");
+      } else if (thisTimeBlockId === presentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+  checkTimeStatus();
 });
