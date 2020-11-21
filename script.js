08b3972495250d1c9f299c943ce66ac0ea600286
David Shonn <davidlshonn@icloud.com>
2020-11-21 18:35:02 +0000

    Set up local srtorage for initial timeblocka

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ $(document).ready(function () {
   var currentDateandTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
   $("#currentDay").text(currentDateandTime);
 
-  
+  $(".saveBtn").on("click", function(){
+      var userCalendarTask = $(this).siblings(".description").val();
+      console.log(userCalendarTask);
+    var timeBlockId = $(this).parent().attr("id");
+   console.log(timeBlockId)
+    localStorage.setItem(timeBlockId, userCalendarTask)
+  })
+
+
+  var taskFor8 = localStorage.getItem("8")
+  $("#8 .description").val(taskFor8);
+  var taskFor9 = localStorage.getItem("9")
+  $("#9 .description").val(taskFor9);
 
 });
