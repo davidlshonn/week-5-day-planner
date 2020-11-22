9667f0cf3585925633c733b81cb71ac143e09f31
David Shonn <davidlshonn@icloud.com>
2020-11-22 15:51:14 +0000

    Comments added to all files

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//This will run once the page DOM is ready to execute JavaScript code
 $(document).ready(function () {
-
+  //Presents a specified format of the time
   var currentDateandTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
   $("#currentDay").text(currentDateandTime);
 
+  //Allows task to be saved by creating a key/value pair(object) within local storage
   $(".saveBtn").on("click", function () {
     var userCalendarTask = $(this).siblings(".description").val();
     var timeBlockId = $(this).parent().attr("id");
     localStorage.setItem(timeBlockId, userCalendarTask);
   });
 
+  //Checks local storage to see if a task has been set, returns it.
   var taskFor8 = localStorage.getItem("8");
   $("#8 .description").val(taskFor8);
   var taskFor9 = localStorage.getItem("9");
@@ -31,8 +34,6 @@ $(document).ready(function () {
   $("#17 .description").val(taskFor9);
   var taskFor8 = localStorage.getItem("18");
   $("#18 .description").val(taskFor8);
-
-
 
   //Checking the time in the time blocks to add the relevant css to them
   function checkTimeStatus() {
